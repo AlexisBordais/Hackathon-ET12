3b124f86e46d48d411181b5fbe8f45449a029627
Sebastien <sbstnplcn@gmail.com>
2016-10-19 16:31:40 +0200

    add bootstrap

--- brunch-config.js
+++ brunch-config.js
@@ -41,6 +41,8 @@ exports.config = {
     plugins: {
         copycat: {
             "js": ['node_modules/angular/angular.min.js', 'node_modules/angular-ui-router/release/angular-ui-router.min.js'],
+            "css": ["bower_components/bootstrap/dist/css/bootstrap.min.css",
+                "bower_components/bootstrap/dist/css/bootstrap.min.css.map"],
             verbose: true,
             onlyChanged: true
         },
