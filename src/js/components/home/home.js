921aad1f8dbd2d3bb18c462f7a8a74a4e5a662ac
Sebastien <sbstnplcn@gmail.com>
2016-10-20 07:31:53 +0200

    home page

--- src/js/components/home/home.js
+++ src/js/components/home/home.js
@@ -3,9 +3,14 @@
     app.component('home', {
         templateUrl: 'js/components/home/home.html',
         controller: function() {
-          angular.extend(this,{
-            
-          })
+          $(document).ready(function() {
+  			$('#fullpage').fullpage({
+  				sectionsColor: ['#f0eee9', '#f0eee9', '#f0eee9', '#f0eee9', '#61935a'],
+  				navigation: true,
+  				navigationPosition: 'right',
+         			autoScrolling: true,
+  			});
+  		});
         }
     })
 })(angular.module('app.home'))
