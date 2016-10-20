a5cd80e09eba5ed4a10425b590d602e661df6700
Sebastien <sbstnplcn@gmail.com>
2016-10-20 04:00:28 +0200

    change cards (sorry audrey)

--- src/js/components/common/modal.js
+++ src/js/components/common/modal.js
@@ -2,7 +2,7 @@
     'use strict'
     app.component("modal", {
         templateUrl: 'js/components/common/modal.html',
-        controller: function() {
+        controller: function($scope, $http) {
             $('.Modal').addClass('Modal--is-open');
             $('.open-modal').hide()
             $('html, body').css('overflow', 'hidden');
@@ -24,6 +24,55 @@
                 $('.open-modal').show()
                 $('html, body').css('overflow', 'auto');
             });
+            $http.get("BordeauxPOI.json")
+                .then(function(r) {
+
+                    $scope.data = r.data
+                    var mapOptions = {
+                        zoom: 15,
+                        center: new google.maps.LatLng(44.84, -0.56) //,
+                            //mapTypeId: google.maps.MapTypeId
+                    }
+
+                    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+                    $scope.markers = [];
+
+                    var imagePath = "http://m.schuepfen.ch/icons/helveticons/black/60/Pin-location.png";
+
+                    for (let i = 0; i < $scope.data.length; i++) {
+                        var data = $scope.data[i]
+
+                        var marker = new google.maps.Marker({
+                            map: $scope.map,
+                            position: new google.maps.LatLng(data.Latitude, data.Longitude),
+
+                            icon: imagePath,
+                            title: data.Nom
+                        })
+                        marker.content = '<div class="infoWindowContent">' + data.Description + '</div>';
+
+                        var infoWindow = new google.maps.InfoWindow();
+
+                        google.maps.event.addListener(marker, 'click', function() {
+                            infoWindow.setContent('<h3>' + this.title + '</h3>' + this.content);
+                            infoWindow.open(this.map, this.marker);
+                        })
+
+
+
+                    }
+                    $scope.openInfoWindow = function(e, selectedMarker) {
+                        e.preventDefault();
+                        google.maps.event.trigger(selectedMarker, 'click');
+                    }
+
+                })
+            $scope.selected = 0;
+
+            $scope.select = function(index) {
+                $scope.selected = index;
+            };
         }
     })
 
