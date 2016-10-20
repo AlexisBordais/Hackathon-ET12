02ac5fd100ad2684dcfadae371258ff1b26d2f34
Audrey <alary.audrey4712@gmail.com>
2016-10-20 02:05:25 +0200

    card

---
+++ src/js/components/common/card.js
@@ -0,0 +1,10 @@
+((app) => {
+    'use strict'
+    app.component("card", {
+        templateUrl: 'js/components/common/card.html',
+        controller: function() {
+              
+          }
+
+    })
+})(angular.module('app.common'))
